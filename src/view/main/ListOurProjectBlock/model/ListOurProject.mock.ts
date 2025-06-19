c2ecb932641cacb7c9511a8d232362b6b9fff7ce
Miro <mirsichkar@gmai.com>
2025-06-19 11:17:56 +0600

    feat/ListOurProject

---
+++ src/view/main/ListOurProjectBlock/model/ListOurProject.mock.ts
@@ -0,0 +1,35 @@
+import { StaticImageData } from "next/image";
+import elite_laptop from  '@shared/imgs/laptop_elite_two.webp'
+import laptop_baytash from  '@shared/imgs/laptop_baytash.webp'
+
+export interface CardProject {
+    main_img:StaticImageData | null
+    img_one:StaticImageData | null
+    img_two:StaticImageData | null
+    title:string
+    description:string[]
+}
+
+export const listOurProject: CardProject[] = [
+    {
+        main_img:elite_laptop,
+        img_one:null,
+        img_two:null,
+        title: 'Elite Academy',
+        description:['Мы помогаем трансформировать систему планирования, организационные процессы, инфраструктуру, а также коммуникационные и материальные ресурсы.','Наша цель — сделать так, чтобы ваша компания уверенно росла и адаптировалась в цифровой среде.']
+    },
+    {
+        main_img:null,
+        img_one:null,
+        img_two:null,
+        title: 'Elite Academy',
+        description:['Мы помогаем трансформировать систему планирования, организационные процессы, инфраструктуру, а также коммуникационные и материальные ресурсы.','Наша цель — сделать так, чтобы ваша компания уверенно росла и адаптировалась в цифровой среде.']
+    },
+    {
+        main_img:null,
+        img_one:null,
+        img_two:null,
+        title: 'Elite Academy',
+        description:['Мы помогаем трансформировать систему планирования, организационные процессы, инфраструктуру, а также коммуникационные и материальные ресурсы.','Наша цель — сделать так, чтобы ваша компания уверенно росла и адаптировалась в цифровой среде.']
+    },
+]
--- src/view/main/index.tsx
+++ src/view/main/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import HeroBlock from './HeroBlock/ui';
 import AboutUs from './AboutUs';
+import ListOurProject from './ListOurProjectBlock/ui';
 
 const Main = () => {
     return (
         <>
             <HeroBlock/>
             <AboutUs/>
+            <ListOurProject/>
         </>
     );
 };
