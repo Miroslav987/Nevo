f229f5d484b3ab9446ece48889fe9a2bc0747089
Miro <mirsichkar@gmai.com>
2025-06-20 13:42:04 +0600

    feat: footer

---
+++ src/entities/footer/model/nav.mock.ts
@@ -0,0 +1,32 @@
+
+
+export interface ItemsNav {
+  title: string;
+  links: Link[];
+}
+
+export interface Link {
+  label: string;
+  path: string;
+}
+
+export const navItems: ItemsNav[] = [
+  {
+    title: "РАЗДЕЛЫ",
+    links: [
+      { label: "Главная", path: "/" },
+      { label: "О нас", path: "/about" },
+      { label: "Наша команда", path: "/team" },
+      { label: "Кейсы работ", path: "/cases" },
+    ],
+  },
+  {
+    title: "ПОЛЕЗНАЯ ИНФОРМАЦИЯ",
+    links: [
+      { label: "FAQ", path: "/faq" },
+      { label: "Оплата", path: "/payment" },
+      { label: "Наши контакты", path: "/contacts" },
+    ],
+  },
+];
+
